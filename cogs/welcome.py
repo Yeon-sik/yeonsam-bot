db4f387d5160901716d63ed5e05099ce50720e7b
yeonchang2 <2191281@hansung.ac.kr>
2026-03-21 20:08:06 +0900

    welcome

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -6,14 +6,29 @@ class Welcome(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        config = get_guild(member.guild.id)
+async def on_member_join(self, member):
+    config = get_guild(member.guild.id)
 
-        message = config["welcome_message"].replace("{server}", member.guild.name)
+    message = config["welcome_message"].replace("{server}", member.guild.name)
 
-        channel = member.guild.system_channel
-        if channel:
-            await channel.send(f"{member.mention} {message}")
+    channel = member.guild.system_channel
+
+    if channel:
+        embed = discord.Embed(
+            title="환영합니다",
+            description=f"{member.mention} {message}",
+            color=0x5865F2
+        )
+
+        await channel.send(
+            embed=embed,
+            view=WelcomeView()
+        )
+
+class WelcomeView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(ProfileButton())
 
 async def setup(bot):
     await bot.add_cog(Welcome(bot))
